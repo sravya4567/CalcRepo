c44b26ec6ea3b7ef5ecb3c5fcc5d388527897837
Pranay Gonipalli <pranaygonipalli1303@gmail.com>
2024-02-16 10:50:33 +0530

    FirstCommit

---
+++ Calc.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcExample
+{
+    public class Calc
+    {
+        public void Add(double n1, double n2)
+        {
+            Console.WriteLine("Result after addition:\t"+(n1 + n2));
+            }
+        public void sub(double n1, double n2)
+        {
+            Console.WriteLine("Result after Subtraction:\t" + (n1 -n2));
+        }
+        public void Multi(double n1, double n2)
+        {
+            Console.WriteLine("Result after Multiplication:\t"+(n1 *n2)); 
+               
+        }
+        public void Div(double n1, double n2)
+        {
+            Console.WriteLine("Result after Divison:\t"+(n1 / n2));
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using CalcExample;
+Calc calc = new Calc();
+Console.WriteLine("Add call");
+calc.Add(12, 45);
+
